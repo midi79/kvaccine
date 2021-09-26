548ae47769f6a4ee74f64133e149dbe56d25f102
Anderson Lee <midi279@gmail.com>
2021-09-26 22:44:47 +0900

    Circuit Break 추가

---
+++ User/src/main/java/kvaccine/external/ReservationServiceImpl.java
@@ -0,0 +1,18 @@
+package kvaccine.external;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReservationServiceImpl implements ReservationService {
+
+	public ArrayList<ReserveDate> dateRequest(Reservation reservation) {
+		
+		System.out.println("################## 조회 폭주로 서비스 지연 중입니다.#######################");
+		System.out.println("################## 조회 폭주로 서비스 지연 중입니다.#######################");
+		System.out.println("################## 조회 폭주로 서비스 지연 중입니다.#######################");
+	
+		return null;
+	}
+}
